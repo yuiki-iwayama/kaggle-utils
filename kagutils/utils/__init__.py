86346353a9b69521aa8a5c36d15f0624eea0c096
yuiki-iwayama <yuiki.iwayama@gmail.com>
2022-05-08 01:11:09 +0900

    utilsのファイル構成を一時作成

---
+++ kagutils/utils/__init__.py
@@ -0,0 +1,1 @@
+from utils.kaggle_access import *
---
+++ kagutils/utils/kaggle_access.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+class Competition:
+    pass
+    # ToDo: Competitionクラスを書く
