01ad9fc1fcbbf03d9e33a730025b25980e2188be
yuiki-iwayama <yuiki.iwayama@gmail.com>
2022-05-08 01:10:32 +0900

    create setup.py

---
+++ kagutils/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="kagutils",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=_requires_from_file("requirements.txt")
+)
