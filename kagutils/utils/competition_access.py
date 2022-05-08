c475d32c1879ce91315b05df69c57679193c5243
yuiki-iwayama <yuiki.iwayama@gmail.com>
2022-05-09 00:45:51 +0900

    docstringの記入

--- kagutils/utils/competition_access.py
+++ kagutils/utils/competition_access.py
@@ -1,38 +1,108 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
+from kaggle import KaggleApi
 import os
 import pandas as pd
 import shutil
 
 
 class Competition(KaggleApi):
+    """
+    The competition class is the child of the KaggleApi class and represents the Kaggle competition.
 
-    def __init__(self, competition_name: str, api_client=None):
+    Attributes:
+        self.competition_id: str
+            competition_id of the competition
+        self.api_client: None default `None`
+            api_client for kaggle.com
+
+    Methods:
+        competition_download_files():
+            Download and unzip the zip containing all competition data files.
+        competition_submit():
+            Convert the pd.DataFrame to a csv file and submit it to the competition.
+    """
+
+    def __init__(self, competition_id, api_client=None):
+        """
+        Constructs all the attributes for the Competition class.
+
+        Parameters:
+            self.competition_id: str
+                competition_id of the competition
+            self.api_client: None default None
+                api_client for kaggle.com
+        """
         super().__init__(api_client=api_client)
-        self.competition_name = competition_name
+        self.competition_id = competition_id
+        self.authenticate()
 
-    @classmethod
-    def api_authenticate(cls, competition_name: str) -> Competition:
-        competition = cls(competition_name)
-        competition.authenticate()
-        return competition
+    def competition_download_files(self, path=None, force=False, quiet=True, save_name="data"):
+        """Download all competition data files
 
-    def competition_download_files(self, path: str=None, force: Bool=False, quiet: Bool=True, save_name: str="datas") -> None:
-        super().competition_download_files(self.competition_name, path, force, quiet)
+        Download and unzip the zip containing all competition data files.
 
-        file_name = self.competition_name + ".zip"
+        Parameters:
+            path: str, default `None`
+                a path to the directory containing the all competition data files to
+            force: bool, default `False`
+                force the download if the file already exists (default False)
+            queit: bool, default `True`
+                suppress verbose output (default is True)
+            save_name:str, default `data`
+                a directory name containing the all competition data files
+
+        Returns
+            None
+
+        Examples:
+            >>> from kagutils.utils import Competition
+            >>> hoge = Competition("hoge")
+            >>> hoge.cometition_download_files()
+        """
+        file_name = self.competition_id + ".zip"
         if path is None:
             file_path = file_name
             save_path = save_name
         else:
             file_path = path + file_name
             save_path = path + save_name
+
+        if os.path.isdir(save_path) and force == False:
+            return None
+
+        super().competition_download_files(self.competition_id, path, force, quiet)
         shutil.unpack_archive(file_path, save_path)
         os.remove(file_path)
 
-        return None
+    def competition_submit(self, df, message, file_name, path=None, quiet=False):
+        """Submit to competition
 
-    def competition_submit(self, df: pd.DataFrame, csv_file_path: str, message: str, quiet: Bool=False) -> None:
-        df.to_csv(csv_file_path, index=False, sep=",")
+        Convert the pd.DataFrame to a csv file and submit it to the competition.
+
+        Parameters
+            df: pd.DataFrame
+                pd.DataFrame containing meta data to be submitted
+            message: str
+                the submission description
+            file_name: str
+                the competition metadata file, extension is always csv
+            path: str, default `None`
+                file path to the competition metadata file
+            quiet: bool, default `False`
+                suppress verbose output (default is False)
+        Returns
+            None
+
+        Examples:
+            >>> from kagutils.utils import Competition
+            >>> import os
+            >>> hoge = Competition("hoge")
+            >>> os.mkdir("./submission")
+            >>> hoge.competition_submit(df, message="first submission", file_name="1st.csv", path="./submission/")
+        """
+        if path is None:
+            file_path = file_name
+        else:
+            file_path = path + file_name
+
+        df.to_csv(file_path, index=False, sep=",")
         super().competition_submit(csv_file_path, message, self.competition_id, quiet)
-
-        return None
