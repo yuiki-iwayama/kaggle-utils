8535e579a6725dfe707368ac963ab7314117e205
yuiki-iwayama <yuiki.iwayama@gmail.com>
2022-05-15 03:42:05 +0900

    設定値の追加

--- kagutils/eda/eda_config.py
+++ kagutils/eda/eda_config.py
@@ -4,4 +4,6 @@ import seaborn as sns
 
 plt.rcParams["axes.titlecolor"] = "black"
 plt.rcParams["text.color"] = "black"
+plt.rcParams["xtick.color"] = "black"
+plt.rcParams["ytick.color"] = "black"
 sns.set()
